a84ba00fb4754ca19020edc410795dc7d02e8f46
soyeolesafm <soyelesafm@hotmail.com>
2023-09-28 22:39:28 -0500

    some arrangements

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,23 @@ import React from "react";
 
 const About = () => {
     return (
-        <div className=" bg-main_light h-screen " id="about" ></div>
+        <div className=" bg-main_light h-screen " id="about" >
+
+            <div className="flex align-middle w-3/4 h-3/4 justify-items-center m-auto md:w-2/3 " >
+            <p className="font-serif text-justify tracking-wide mt-28 m-auto md:text-lg" > 
+            I'm a web developer with a diverse skill set that encompasses JavaScript, ReactJS,
+            and Tailwind CSS. My journey into the world of programming began in high school,
+            where I first honed my coding skills with Basic. Additionally,
+            I have a background in graphic design, adding a creative dimension to my
+            expertise. I'm not only dedicated and hardworking but also thrive in high-pressure
+            situations, maintaining a calm and composed demeanor.
+            My organizational skills are top-notch, and I excel in troubleshooting and 
+            finding innovative solutions to complex challenges. 
+            I'm excited to contribute my skills and passion to your team and take on new web development challenges
+            </p>
+            </div>
+            
+        </div>
     )
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+// import  Logoesa  from "./img/logocara.svg"
 const Card = () => {
     return (
         <div className='   grid grid-rows-3 grid-cols-3 grid-flow-col gap-4 bg-paper_dark drop-shadow-md rounded-md h-40 w-5/6 pr-8 m-auto md:h-60 md:w-auto   ' >
-            <div className=' row-span-3  bg-ink rounded-md'  ></div>
+            <div className=' row-span-3 '  > 
+            {/* <img src={Logoesa} /> */}
+            </div>
             <h1 className=' row-span-2 col-span-2  text-ink font-cursive text-5xl text-center mt-8 m-auto md:text-left md:text-8xl  md:mt-24  ' > Esaul Flores </h1>
             <h3 className=' col-span-2  text-ink font-sans text-xl text-center mt-0 tracking-wider md:text-4xl md:text-left md:mt-1 '  > Web Developer </h3>
 
