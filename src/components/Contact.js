749318daa6e8adf817ec8adabbb54b484950ebea
soyeolesafm <soyelesafm@hotmail.com>
2024-01-04 18:19:29 -0600

    concatc changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,32 @@
+import {ReactComponent as Logoesa} from "./assets/logo.svg"
 import React from "react";
 
 const Contact = () => {
     return (
-        <div className=" bg-main_light h-screen " id="contact" ></div>
+        <div className=" bg-main_light h-screen " id="contact" >
+            <div className=" flex flex-col lg:flex-row h-screen " >
+                <div className=" lg:w-1/2 lg:h-full p-8 order-2 lg:order-1 " >
+                    <div className="mb-4">
+                        <Logoesa className="mb-2 w-14 h-auto " />
+                        <a href="#" className=" block ">linkedin</a>
+
+                    </div>
+                    <div>
+                        <Logoesa className="mb-2 w-14 h-auto  " />
+                        <a href="#" className="block" >github</a>
+                    </div>
+                    <div className=" lg:w-1/2 lg:h-full p-8 order-1 lg:order-2">
+                        <form className="h-full flex flex-col justify-center">
+                            <div className="mb-4" >
+
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
     )
 }
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,18 +2,23 @@ import React from "react";
 import dalzo from './assets/Dalzo.png'
 import tictactoe from './assets/tictactoe.jpg'
 import calculator from './assets/calculator.jpg'
+import wide_calculator from './assets/wide_calc.png'
+import wide_tictactoe from './assets/wide_tictactoe.png'
 const Projects = () => {
     return (
-        <div className=" flex flex-col justify-center bg-paper h-screen mb-5 md:flex-row " id="projects" >
+        <div className=" flex flex-col justify-center bg-paper h-screen  md:flex-row " id="projects" >
             <div className=" m-auto mt-16" >
                 <img  src={dalzo} className="w-11/12 m-auto mt-16" alt="dalzo group" />
             </div  >
             <div className=" flex flex-row gap-6 m-auto mt-1 md:flex-col ">
                 <a className=" ml-5" href="https://soyeolesafm.github.io/tictactoe/" target="blank" > 
-                    <img src= {tictactoe} className=" w-full m-auto md:h-3/6 md:w-2/5 " alt="tic tac toe" />
+                    <img src= {wide_tictactoe} className=" hidden md:flex md:w-4/6 md:m-auto  " alt="tic tac toe" />
+                    <img src= {tictactoe} className=" md:hidden w-full m-auto md:h-3/6  " alt="tic tac toe" />
                 </a>
                 <a className=" mr-5" href="https://soyeolesafm.github.io/calculator/" target="blank" >
-                    <img src={calculator} className=" w-full m-auto md:h-3/6 md:w-2/5 " alt="calculator " />
+                    <img src= {wide_calculator} className=" hidden md:flex md:w-4/6 md:m-auto  " alt="calculator" />
+                    <img src={calculator} className="md:hidden  w-full m-auto md:h-3/6 md:w-2/5 " alt="calculator " />
+
                 </a>
                 
             </div>
