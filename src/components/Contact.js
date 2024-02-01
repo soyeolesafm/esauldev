d948fd69773881c6e1ba06b11ec2a67aa41d3a72
soyeolesafm <soyelesafm@hotmail.com>
2024-02-01 14:04:54 -0600

    fixing contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,28 +3,61 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <div className=" bg-main_light h-screen " id="contact" >
+        <div className=" bg-paper_dark h-screen " id="contact" >
             <div className=" flex flex-col lg:flex-row h-screen " >
                 <div className=" lg:w-1/2 lg:h-full p-8 order-2 lg:order-1 " >
-                    <div className="mb-4">
-                        <Logoesa className="mb-2 w-14 h-auto " />
-                        <a href="#" className=" block ">linkedin</a>
+                    <div className="mb-4 justify-center">
+                        <Logoesa className=" m-auto mb-2 w-14 h-auto " />
+                        <a href="#" className=" block  text-center ">linkedin</a>
 
                     </div>
                     <div>
-                        <Logoesa className="mb-2 w-14 h-auto  " />
+                        <Logoesa className=" m-auto mb-2 w-14 h-auto  " />
                         <a href="#" className="block" >github</a>
                     </div>
-                    <div className=" lg:w-1/2 lg:h-full p-8 order-1 lg:order-2">
+                    
+                </div>
+                <div className=" lg:w-1/2 lg:h-full p-8 order-1 lg:order-2">
                         <form className="h-full flex flex-col justify-center">
                             <div className="mb-4" >
+                                <label className="block text-main_dark text-sm font-bold mb-2" htmlFor="name">Name:</label>
+                                <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-outline focus:shadow-outline"
+                                id='name'
+                                type="text"
+                                placeholder="your name"
+                                />
+                                <div className="mb-4">
+                                    <label className="block text-main_dark text-sm font-bold mb-2" htmlFor="email">Email:</label>
+                                    <input 
+                                        className="shadow appearance-none border rounded w-full py-2 px-3 text-main_dark leading-tight focus:outline-none focus:shadow-outline"
+                                        id="email"
+                                        type="email"
+                                        placeholder="your email"
+                                    />
+                                </div>
+                                <div className="mb-4">
+                                    <label className="block text-main_dark text-sm font-bold mb-2" htmlFor="message">Message:</label>
+                                    <textarea 
+                                        className="shadow appearance-none border rounded w-full py-2 px-3 text-main_dark leading-tight focus:outline-none focus:shadow_outline"
+                                        id="message"
+                                        rows={4}
+                                        placeholder="Message"
+                                    />
+                                </div>
+                                <div>
+                                <button
+                                    className=" bg-main_dark hover:bg-main_light text-paper font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                    type="submit"
+                                >
+                                    Send
+                                </button>
+                                </div>
 
                             </div>
 
                         </form>
-                    </div>
-                </div>
-                
+                    </div>                
             </div>
         </div>
     )
